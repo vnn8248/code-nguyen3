2b3778969ff08c5584170945840abd35908652cb
Bibi Westside <vnn8248@gmail.com>
2021-02-09 10:51:47 -0500

    Update env context in next.config.js

--- next.config.js
+++ next.config.js
@@ -11,14 +11,16 @@ module.exports = (phase, { defaultConfig }) => {
         CN_API_URL: "http://localhost:5000/"
       }
     }
-  } 
-  return {
-    // prod config options
-    env: {
-      CONTENT_API_KEY: "db12fc51133acab5c30a5c6fc6",
-      CONTENT_API_URL: "https://code-nguyen3-ghost.herokuapp.com",
-      CN_API_URL: "https://blooming-sands-31061.herokuapp.com/"
-    }
-  }  
+  } else {
+    return {
+      // prod config options
+      env: {
+        CONTENT_API_KEY: "db12fc51133acab5c30a5c6fc6",
+        CONTENT_API_URL: "https://code-nguyen3-ghost.herokuapp.com",
+        CN_API_URL: "https://blooming-sands-31061.herokuapp.com/"
+      }
+    }  
+  }
+
 }
 
