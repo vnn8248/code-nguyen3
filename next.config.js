f7448ae3490109ce10c8a53d6e02201acbc5590b
Bibi Westside <vnn8248@gmail.com>
2021-02-09 10:24:42 -0500

    Add env configs and vars to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,24 @@
-module.exports = () => {
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants'); 
 
+module.exports = (phase, { defaultConfig }) => {
+
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      // dev config options
+      env: {
+        CONTENT_API_KEY: "cacf4869a36aef0055a16a2c48",
+        CONTENT_API_URL: "http://localhost:2368",
+        NODE_API_URL: "http://localhost:5000/"
+      }
+    }
+  } 
   return {
-    // config options here
+    // prod config options
     env: {
-      CONTENT_API_KEY: "cacf4869a36aef0055a16a2c48",
-    },
-    images: {
-      domains: ["https://static.ghost.org"],
-    },
-  }
+      CONTENT_API_KEY: "db12fc51133acab5c30a5c6fc6",
+      CONTENT_API_URL: "https://code-nguyen3-ghost.herokuapp.com",
+      NODE_API_URL: "https://blooming-sands-31061.herokuapp.com/"
+    }
+  }  
 }
+
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 // Components
 import Head from 'next/head';
 import Navbar from "../components/Navbar";
-import Header from "../components/header";
+import Header from "../components/Header";
 import About from "../components/About";
 import Resume from "../components/Resume";
 import Portfolio from "../components/Portfolio";
