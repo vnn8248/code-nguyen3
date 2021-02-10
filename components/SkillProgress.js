6c3c5d08c2d102a5fecb2c3baac3fdbf4fbbeb0c
Bibi Westside <vnn8248@gmail.com>
2021-02-09 19:43:45 -0500

    Move image src template literal to const outside render

--- components/SkillProgress.js
+++ components/SkillProgress.js
@@ -2,9 +2,10 @@ import styles from "../styles/modules/SkillProgress.module.scss";
 import Image from "next/image";
 
 function SkillProgress(props) {
+  const logo = `/svg/${props.name}.svg`;
   return (
     <li className={styles.skillRow}>
-      <Image src={`/svg/${props.name}.svg`} alt={props.alt} width={50} height={50} className={styles.logo}/>
+      <Image src={logo} alt={props.alt} width={50} height={50} />
       <h3>{props.name}</h3>
       <div className={styles.progress}>
         <div style={{width: `${props.now}%`, backgroundColor: `${props.color}`}}></div>
