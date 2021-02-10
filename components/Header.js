519d737f00a31f474c5318a49bbf45d3d1dd8d6e
Bibi Westside <vnn8248@gmail.com>
2021-02-09 20:28:47 -0500

    Testing image component

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import Navbar from "./Navbar";
 import SocialIcons from "./SocialIcons";
 import styles from "../styles/modules/Header.module.scss";
@@ -8,7 +9,7 @@ function Header() {
     <header className={styles.hero}>
 
         <div className={styles.content}>
-          <img src="/svg/avataaars-bn.svg" className={styles.avatar}></img>
+          <Image src="/svg/avataaars-bn.svg" className={styles.avatar} width={100} height={100}/>
           <h1>Hi, I'm Bi Nguyen.</h1>
           <p>I'm a full stack web developer.</p>
           <p>Welcome to my online portfolio.</p>
